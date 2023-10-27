baa1f926c6621d1162edc6e9b4687e30984c687c
Jimmy-Rais <raisgachaba@gmail.com>
2023-10-27 14:41:40 +0530

    27/10/2023

--- watchmate/watchlist_app/api/views.py
+++ watchmate/watchlist_app/api/views.py
@@ -2,11 +2,20 @@ from watchlist_app.models import Movie
 from watchlist_app.api.serialzers import MovieSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-@api_view()
+@api_view(['GET','POST'])
 def movie_list(request):
-   movies=Movie.objects.all()
-   serialazer=MovieSerializer(movies)
-   return Response(serialazer.data)
+   if request.method == 'GET':
+   
+     movies=Movie.objects.all()
+     serialazer=MovieSerializer(movies,many=True)
+     return Response(serialazer.data)
+   if request.method == 'POST':
+      serializer=MovieSerializer(data=request.data)
+      if serializer.is_valid():
+       serializer.save()
+       return Response(serializer.data)
+      else:
+       return Response(serialazer.errors)
 @api_view()
 def movie_details(request,pk):
     movie=Movie.objects.get(pk=pk)
